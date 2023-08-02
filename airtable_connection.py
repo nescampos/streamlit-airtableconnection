22966ad8ebea56e2886d35d762efa480697d9326
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2023-08-02 15:32:23 -0400

    Add files via upload

--- airtable_connection.py
+++ airtable_connection.py
@@ -13,19 +13,16 @@ class AirtableConnection(ExperimentalBaseConnection[Api]):
             access_token = self._secrets['access_token'] 
         return Api(access_token)
 
-    def api(self) -> Api:
-        return self._instance()
-    
     
     def get_all(self, base_id, table_name, ttl: int = 3600) -> List[RecordDict]:
-        table = self.api().table(base_id, table_name)
+        table = self._instance.table(base_id, table_name)
         records = table.all()
         return records
 
     def get(self, base_id, table_name, record_id, ttl: int = 3600) -> RecordDict:
         @cache_data(ttl=ttl)
         def _get(base_id, table_name, record_id):
-            table = self._instance().table(base_id, table_name)
+            table = self._instance.table(base_id, table_name)
             record = table.get(record_id)
             return record
 
@@ -33,32 +30,32 @@ class AirtableConnection(ExperimentalBaseConnection[Api]):
 
 
     def create(self, base_id, table_name, record) -> RecordDict:
-        table = self._instance().table(base_id, table_name)
+        table = self._instance.table(base_id, table_name)
         record = table.create(record)
         return record
 
     def create_many(self, base_id, table_name, records) -> List[RecordDict]:
-        table = self._instance().table(base_id, table_name)
+        table = self._instance.table(base_id, table_name)
         records = table.batch_create(records)
         return records
 
     def update(self, base_id, table_name, record_id, fields, replace=False) -> RecordDict:
-        table = self._instance().table(base_id, table_name)
+        table = self._instance.table(base_id, table_name)
         record = table.update(record_id, fields, replace=replace)
         return record
 
     def update_many(self, base_id, table_name, records, replace=False) -> List[RecordDict]:
-        table = self._instance().table(base_id, table_name)
+        table = self._instance.table(base_id, table_name)
         records = table.batch_update(records, replace=replace)
         return records
 
     def delete(self, base_id, table_name, record_id) -> RecordDeletedDict:
-        table = self._instance().table(base_id, table_name)
+        table = self._instance.table(base_id, table_name)
         record = table.delete(record_id)
         return record
 
     def delete_many(self, base_id, table_name, records) -> List[RecordDeletedDict]:
-        table = self._instance().table(base_id, table_name)
+        table = self._instance.table(base_id, table_name)
         records = table.batch_delete(records)
         return records
         
